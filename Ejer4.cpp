e1ff9930e9b905f0367f66de9aac51c78847df6e
Kevduran <00088020@uca.edu.sv>
2020-05-08 22:02:21 -0600

    Ejer4

---
+++ Ejer4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num;
+
+    cout<<"Digite su numero: ";
+    cin>>num;
+
+    if (num%2==0){
+
+        cout<<"Su numero es par";
+    }
+    else{
+        cout<<"Su numero es impar";
+    }
+
+
+    return 0;
+}
